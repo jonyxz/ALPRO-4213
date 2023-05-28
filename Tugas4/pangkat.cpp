2d086b5973c703310b3b40323e8a72e6287ec8ce
jonyxz <118349343+jonyxz@users.noreply.github.com>
2023-05-28 16:08:31 +0700

    Update pangkat.cpp
    add declaration

--- Tugas4/pangkat.cpp
+++ Tugas4/pangkat.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int pangkatrec(int a, int b){
-    if (b==0)
-        return 1;
-    else
-        return a* pangkatrec(a,b-1);
-}
+int pangkatrec(int a, int b);
 
 int main(){
     int a,b;
@@ -16,4 +11,11 @@ int main(){
     cin >> b;
     cout << "hasil "  << a << " pangkat " << b << " : "  << pangkatrec(a,b);
     return 0;
+}
+
+int pangkatrec(int a, int b){
+    if (b==0)
+        return 1;
+    else
+        return a* pangkatrec(a,b-1);
 }
